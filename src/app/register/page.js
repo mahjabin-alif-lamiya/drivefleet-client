7621d2bad30274709b80051acf7d83c76968c0d7
mahjabin-alif-lamiya <alifmahjabin032@gmail.com>
2026-09-05 14:13:33 +0600

    Point register page to deployed server URL

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -29,7 +29,7 @@ export default function RegisterPage() {
     }
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/register', {
+      const res = await fetch('https://drivefleet-server-hvcw.onrender.com/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
